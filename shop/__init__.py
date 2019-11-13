95ac17c1f0e99d1faa74ee9476bbe35564ceb630
Fineax <michal.kanarek@gmail.com>
2019-11-13 22:17:28 +0100

    Add ability to add product to cart

--- shop/__init__.py
+++ shop/__init__.py
@@ -2,24 +2,33 @@ from pymongo import MongoClient
 import os
 import base64
 
+
 class Shop:
-    def products_db(self):
+    def shop_db(self):
         myclient = MongoClient(
             f"mongodb+srv://{os.getenv('DB_NAME')}:{os.getenv('PASS')}@cluster0-twyu7.mongodb.net/test?retryWrites=true&w=majority")
         shop_db = myclient["Shop_db"]
+        return shop_db
+
+    def products_db(self):
+        shop_db = self.shop_db()
         products = shop_db["products"]
         return products
 
+    def cart_db(self):
+        shop_db = self.shop_db()
+        carts = shop_db["carts"]
+        return carts
 
-    def add_poduct_to_db(self,name,description, category="phone" ,image_path="1.jpg"):
+    def add_poduct_to_db(self, name, price, description, category="phone", image_path="1.jpg"):
         encoded_file = self.image_file_encode(image_path)
         db = self.products_db()
-        db.insert_one({"category": category, "name": name, "description": description, "image_file": encoded_file})
-
-
-    def image_file_encode(self, image_path="1.jpg"):
+        db.insert_one({"category": category, "name": name, "price": price, "description": description, "image_file": encoded_file})
+    @staticmethod
+    def image_file_encode(image_path="1.jpg"):
         with open(image_path, "rb") as myfile:
             data = myfile.read()
             return base64.b64encode(data)
 
-
+    def add_product_to_cart(self, user_id, product_id, quantity):
+        self.cart_db().update_one({"user_id": user_id}, {"$push": {"products": [product_id, quantity]}}, upsert=True)
--- tests/shop_test.py
+++ tests/shop_test.py
@@ -24,12 +24,12 @@ def test_shop_add_to_db():
     products = shop.products_db()
     current_quantity = products.count_documents({"name": "samsung"})
     try:
-        shop.add_poduct_to_db("samsung", "S10")
+        shop.add_poduct_to_db("samsung", 20, "S10")
         if not (products.count_documents({"name": "samsung"}) > current_quantity):
             raise SyntaxError
         products.delete_one({"name": "samsung", "description": "S10"})
         assert 1
-    except:
+    except SyntaxError:
         assert 0
 
 
@@ -38,3 +38,17 @@ def test_shop_image_file_encode():
         data = myfile.read()
     encoded = Shop().image_file_encode()
     assert base64.b64decode(encoded) == data
+
+def test_shop_add_product_to_card():
+    shop = Shop()
+    carts = shop.cart_db()
+    try:
+        shop.add_product_to_cart(9876, 12344, 24)
+        carts_length = len(carts.find({"user_id": 9876}).next()["products"])
+        shop.add_product_to_cart(9876, 12344, 24)
+        if not len(carts.find({"user_id": 9876}).next()["products"])>carts_length:
+            raise AssertionError
+        carts.delete_one({"user_id": 9876})
+        assert True
+    except AssertionError:
+        assert False
